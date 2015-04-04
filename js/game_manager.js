a12767f3339d2f177a7b019ea38a82f35b00b5ac
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2015-04-04 05:39:46 +0530

    5120 game completed, blue colors

--- js/game_manager.js
+++ js/game_manager.js
@@ -68,7 +68,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 2 : 4;
+    var value = Math.random() < 0.9 ? 5 : 10;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -166,8 +166,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value;
 
-          // The mighty 2048 tile
-          if (merged.value === 2048) self.won = true;
+          // The mighty 5120 tile
+          if (merged.value === 5120) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
